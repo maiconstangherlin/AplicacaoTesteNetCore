85721afcce1988ebf5a678988abd4932fb127d01
Maicon Stangherlin <maicon.stangherlin@gmail.com>
2021-06-28 22:14:55 -0300

    Project UniversatyCoin started

---
+++ UniCoin/Controllers/StudentyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniCoin.Models;
+
+namespace UniCoin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentyController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("/{id}")]
+        public ActionResult GetById(long id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Add([FromBody] Studenty student)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = student.Id }, student);
+        }
+
+        [HttpDelete("/{id}")]
+        public ActionResult delete(long id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ UniCoin/Models/Studenty.cs
@@ -0,0 +1,11 @@
+﻿namespace UniCoin.Models
+{
+    public class Studenty
+    {
+
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
